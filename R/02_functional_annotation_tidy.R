8a37187da04a16409f465069d3a08e96d2c1f56a
Rebecca Garner <rebecca.garner@mail.concordia.ca>
2019-05-23 12:22:16 -0400

    Updated functional annotation scripts 02 and 03

--- R/02_functional_annotation_tidy.R
+++ R/02_functional_annotation_tidy.R
@@ -10,34 +10,36 @@ taxon_oid <- "3300032269"
 
 # Import IMG .depth.txt file containing mean depth of coverage per scaffold
 img_depth <- read_tsv(file = paste0(taxon_oid, ".a.depth.txt"), quote = "",
-                      col_names = TRUE, progress = TRUE)
+                      col_names = TRUE)
+
+# Function to extract IMG scaffold IDs embedded in gene IDs
+extract_scaffold_id <- function(gene_id) {
+  scaffold_id <- substr(x = gene_id, start = 1, stop = nchar(img_depth$ID[1]))
+  return(scaffold_id)
+}
 
 # Import IMG .cog.txt file which contains gene product information
 img_cog <- read_tsv(file = paste0(taxon_oid, ".a.cog.txt"),
                     col_names = c("gene_id", "cog_id", "percent_identity",
                                   "align_length", "query_start", "query_end",
                                   "subj_start", "subj_end", "evalue",
-                                  "bit_score"), quote = "", progress = TRUE)
+                                  "bit_score"),
+                    quote = "") %>%
+  mutate(scaffold_id = extract_scaffold_id(gene_id))
 
 # Import IMG .ko.txt file which contains gene product information
+# Remove string "KO:" from KO term
 img_ko <- read_tsv(file = paste0(taxon_oid, ".a.ko.txt"),
                    col_names = c("gene_id", "img_ko_flag", "ko_term",
                                  "percent_identity", "query_start", "query_end",
                                  "subj_start", "subj_end", "evalue", "bit_score",
-                                 "align_length"), quote = "", progress = TRUE)
-
-# Remove string "KO:" from KO term
-img_ko <- img_ko %>%
-  mutate(ko_term = gsub(pattern = "KO:", replacement = "", x = ko_term))
-
-# Extract IMG scaffold IDs embedded in gene IDs
-extract_scaffold_id <- function(gene_id) {
-  scaffold_id <- substr(x = gene_id, start = 1, stop = nchar(img_depth$ID[1]))
-  return(scaffold_id)
-}
-img_cog <- img_cog %>% mutate(scaffold_id = extract_scaffold_id(gene_id))
+                                 "align_length"),
+                   quote = "") %>%
+  mutate(ko_term = gsub(pattern = "KO:", replacement = "", x = ko_term)) %>%
+  mutate(scaffold_id = extract_scaffold_id(gene_id))
+  
+# Join COG, KO tables with scaffold coverage information
 img_cog <- left_join(x = img_cog, y = img_depth, by = c("scaffold_id" = "ID"))
-img_ko <- img_ko %>% mutate(scaffold_id = extract_scaffold_id(gene_id))
 img_ko <- left_join(x = img_ko, y = img_depth, by = c("scaffold_id" = "ID"))
 
 # Import COGs table
--- R/03_functional_annotation_compare.R
+++ R/03_functional_annotation_compare.R
@@ -1,13 +1,29 @@
+# Functional annotations for > 1 metagenome
 
 library(tidyverse)
 
+# Enter path to directory where IMG downloads are saved
 img_file_path <- "C:/Users/Gandalf/Desktop/img_files/"
+
 img_depth_files <- dir(path = img_file_path, pattern = "*.a.depth.txt")
 
+# Import and combine all IMG depth files into 1 data frame
+# Assign taxon OID and GOLD analysis project ID
 img_depth <- img_depth_files %>%
   map_dfr(function(x) {
     read_tsv(file.path(img_file_path, x)) %>%
       mutate(taxon_oid = gsub(pattern = ".a.depth.txt", replacement = "", x = x))
   }) %>%
   mutate(analysis_project_id = gsub(pattern = "_.*", replacement = "", x = ID))
+
+
+######### For scaffold ID extraction, make sure to include info that scaffold ids may be of differing length between analysis projects
+
+
+# Import IMG .cog.txt file which contains gene product information
+img_cog <- read_tsv(file = paste0(taxon_oid, ".a.cog.txt"),
+                    col_names = c("gene_id", "cog_id", "percent_identity",
+                                  "align_length", "query_start", "query_end",
+                                  "subj_start", "subj_end", "evalue",
+                                  "bit_score"), quote = "", progress = TRUE)
 
