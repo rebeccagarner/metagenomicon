bc2175dd901ee2cd8d613ab2937f8fb041982e4c
Rebecca Garner <rebecca.garner@mail.concordia.ca>
2019-05-25 17:10:26 -0400

    Updated COG files import and scaffold ID extraction in 03_functional_annotation_compare.R

--- R/03_functional_annotation_compare.R
+++ R/03_functional_annotation_compare.R
@@ -2,28 +2,55 @@
 
 library(tidyverse)
 
+# Enter path to metagenomicon repo
+setwd("PATH/metagenomicon/")
+
+
 # Enter path to directory where IMG downloads are saved
 img_file_path <- "C:/Users/Gandalf/Desktop/img_files/"
 
-img_depth_files <- dir(path = img_file_path, pattern = "*.a.depth.txt")
 
-# Import and combine all IMG depth files into 1 data frame
+# Import and combine IMG depth files
 # Assign taxon OID and GOLD analysis project ID
+(img_depth_files <- dir(path = img_file_path, pattern = "*.a.depth.txt"))
+
 img_depth <- img_depth_files %>%
   map_dfr(function(x) {
     read_tsv(file.path(img_file_path, x)) %>%
       mutate(taxon_oid = gsub(pattern = ".a.depth.txt", replacement = "", x = x))
   }) %>%
-  mutate(analysis_project_id = gsub(pattern = "_.*", replacement = "", x = ID))
+  rename(scaffold_id = ID) %>%
+  mutate(project_id = gsub(pattern = "_.*", replacement = "", x = scaffold_id))
 
 
-######### For scaffold ID extraction, make sure to include info that scaffold ids may be of differing length between analysis projects
+# Extract IMG scaffold ID string lengths
+img_scaffold_ids <- img_depth %>%
+  distinct(taxon_oid, project_id, .keep_all = TRUE) %>%
+  mutate(scaffold_id_length = str_length(scaffold_id)) %>%
+  select(-c(scaffold_id, AvgFold))
 
 
-# Import IMG .cog.txt file which contains gene product information
-img_cog <- read_tsv(file = paste0(taxon_oid, ".a.cog.txt"),
-                    col_names = c("gene_id", "cog_id", "percent_identity",
-                                  "align_length", "query_start", "query_end",
-                                  "subj_start", "subj_end", "evalue",
-                                  "bit_score"), quote = "", progress = TRUE)
+# Import and combine IMG COG files
+(img_cog_files <- dir(path = img_file_path, pattern = "*.a.cog.txt"))
 
+img_cog <- img_cog_files %>%
+  map_dfr(function(x) {
+    read_tsv(file.path(img_file_path, x),
+             col_names = c("gene_id", "cog_id", "percent_identity", "align_length",
+                           "query_start", "query_end", "subj_start", "subj_end",
+                           "evalue", "bit_score"),
+             cols_only(gene_id = col_character(), cog_id = col_character())) %>%
+      mutate(taxon_oid = gsub(pattern = ".a.cog.txt", replacement = "", x = x))
+  }) %>%
+  left_join(img_scaffold_ids, by = "taxon_oid") %>%
+  mutate(scaffold_id = substr(gene_id, start = 1, stop = scaffold_id_length)) %>%
+  select(-c(taxon_oid, project_id, scaffold_id_length)) %>%
+  left_join(img_depth, by = "scaffold_id")
+
+
+# Import GOLD analysis projects summary
+img_projects <- read_tsv(file = "data/gold_analysis_projects.txt", col_names = TRUE) %>%
+  select(-`Add Date`) %>%
+  rename(project_id = `GOLD Analysis Project ID`,
+         project_name = `Analysis Project Name`,
+         project_type = `Analysis Project Type`)
